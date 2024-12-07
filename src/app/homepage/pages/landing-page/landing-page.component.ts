ed8f660fa022de3c2020aa9b44c401ff971f3fd5
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-12-07 11:53:58 +0100

    implémentation des sections 7 'estimation gratuite' et section 8 'nos conseils et actualité'

--- src/app/homepage/pages/landing-page/landing-page.component.ts
+++ src/app/homepage/pages/landing-page/landing-page.component.ts
@@ -4,6 +4,14 @@ import {
   faArrowUpRightFromSquare,
 } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import {
+  faHouse,
+  faBuilding,
+  faStore,
+  faLandmark,
+  faHotel,
+  faWarehouse,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
@@ -15,6 +23,14 @@ import { faHeart } from '@fortawesome/free-regular-svg-icons';
 export class LandingPageComponent {
   backgroundImage =
     'https://images.unsplash.com/photo-1519354754184-e1d9c46182c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+  // Icons
+  faHouse = faHouse; // Maison
+  faBuilding = faBuilding; // Immeuble
+  faStore = faStore; // Fond de commerce
+  faLandmark = faLandmark; // Terrain
+  faHotel = faHotel; // Appartement
+  faWarehouse = faWarehouse; // Local commercial
   faDoorOpen = faDoorOpen;
   faHeart = faHeart;
   faArrowUpRightFromSquare = faArrowUpRightFromSquare;
@@ -51,6 +67,33 @@ export class LandingPageComponent {
       title: 'Trouvez le logement de vos rêves',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec odio non lacus fringilla dignissim quis vel diam. Integer convallis purus odio, vitae semper leo posuere ac.',
+    },
+  ];
+
+  articles = [
+    {
+      title: 'Les meilleures stratégies pour investir dans l’immobilier',
+      description:
+        'Découvrez les astuces et conseils pour maximiser vos investissements immobiliers.',
+      image: 'https://via.placeholder.com/300x200', // URL di immagine fittizia
+    },
+    {
+      title: 'Les tendances actuelles du marché immobilier sous l’équateur',
+      description:
+        'Un aperçu des dernières tendances et opportunités dans le secteur.',
+      image: 'https://via.placeholder.com/300x200',
+    },
+    {
+      title: 'Comment choisir le bon type de propriété',
+      description:
+        'Les clés pour choisir entre maison, appartement ou local commercial.',
+      image: 'https://via.placeholder.com/300x200',
+    },
+    {
+      title: 'Les avantages fiscaux pour les propriétaires en 2024',
+      description:
+        'Découvrez les aides et les réductions fiscales disponibles cette année.',
+      image: 'https://via.placeholder.com/300x200',
     },
   ];
 }
